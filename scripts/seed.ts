bec72ccd1c5883406a12e8579b37a44733e3dc19
Lenson <53988015+Lebarnon@users.noreply.github.com>
2024-03-18 00:21:10 +0800

    Update seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,24 +4,29 @@ const db = new PrismaClient();
 
 async function main() {
     try{
-        await db.category.createMany({
-            data: [
-                { name: "Food" },
-                { name: "Clothing" },
-                { name: "Electronics" },
-                { name: "Books" },
-                { name: "Home" },
-                { name: "Garden" },
-                { name: "Health" },
-                { name: "Beauty" },
-                { name: "Toys" },
-                { name: "Sports" }
-            ]
-        });
+        
     }catch(error){
         console.error("Error seeding default categories: ", error);
     }finally{
         await db.$disconnect();
     }
 }
+
+async function categorySeed() {
+    await db.category.createMany({
+        data: [
+            { name: "Food" },
+            { name: "Clothing" },
+            { name: "Electronics" },
+            { name: "Books" },
+            { name: "Home" },
+            { name: "Garden" },
+            { name: "Health" },
+            { name: "Beauty" },
+            { name: "Toys" },
+            { name: "Sports" }
+        ]
+    });
+}
+
 main()
